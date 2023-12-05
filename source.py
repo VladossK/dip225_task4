a047bf2b8b07176d7cc1e29e35b00d5bef80356b
Vladossmmf <144938705+Vladossmmf@users.noreply.github.com>
2023-12-05 04:17:35 +0200

    05.12.2023
    Not completed task but close to do it

--- source.py
+++ source.py
@@ -11,12 +11,36 @@ driver = webdriver.Chrome(service=service, options=option)
 
 name=[]
 # program read information from people.csv file and put all data in name list.
-with open("people.csv", "r") as file:
-    next(file)
-    for line in file:
-        row=line.rstrip().split(",") 
-        name.append(row)
+with open("people.csv", "r") as f:
+    next(f)
+    for file in f:
+        row=file.rstrip().split(",")
+        fname=[row[2], row[3]]
+        fname_str=" ".join(fname)
+        name.append(str(fname_str))
+
+
+
+#print(name)
 
 url = "https://emn178.github.io/online-tools/crc32.html"
 driver.get(url)
 time.sleep(2)
+
+
+
+
+
+namesUnc = []
+for names in name:
+        find1 = driver.find_element(By.ID, "input")
+        last = find1.send_keys(names)    
+        find = driver.find_element(By.ID, "output")
+        temp = find.get_attribute("value")
+        namesUnc.append(temp)
+        find1.clear()
+
+print(namesUnc)
+
+    
+    
